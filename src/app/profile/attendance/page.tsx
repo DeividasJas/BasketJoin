65d7c2488c3949dccfa7a4e40b490f7a3b0e3551
Deividas Jasas <jasas.code@gmail.com>
2024-12-20 21:42:49 +0000

    Bug fixes

--- src/app/profile/attendance/page.tsx
+++ src/app/profile/attendance/page.tsx
@@ -4,6 +4,7 @@ export default function Page() {
       <section className="mt-6 flex h-full w-full max-w-[700px] flex-col">
         <h2 className="mb-2 text-center text-xl">Attendance</h2>
         <p></p>
+        <p></p>
       </section>
     </>
   );
