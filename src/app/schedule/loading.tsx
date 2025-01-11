a4559de068b443cfc1f33bce81d435aa4eda30fb
Deividas Jasas <jasas.code@gmail.com>
2025-01-11 16:50:54 +0200

    Fix: schedule skeleton width

--- src/app/schedule/loading.tsx
+++ src/app/schedule/loading.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 
 export default function ScheduleSkeleton() {
   return (
-    <div className="mt-10 rounded-md bg-zinc-900 px-2 py-6">
+    <div className="mx-auto mt-10 max-w-[900px] rounded-md bg-zinc-900 px-2 py-6">
       <h1 className="text-center text-3xl font-bold">Schedule</h1>
       <h3 className="my-4 ml-[20px] text-lg">Next game starts in:</h3>
 
@@ -14,15 +14,11 @@ export default function ScheduleSkeleton() {
         <Skeleton className="flex aspect-square min-h-full w-[4.5rem] max-w-24 basis-1 flex-col items-center justify-center rounded-md bg-zinc-700 py-3 sm:w-20" />
       </div>
       <div className="ml-[20px] mt-4 flex gap-2">
-        {/* <Skeleton className="h-[36px] w-[87.14px] bg-zinc-700" /> */}
         <RegistrationBtn />
         <Skeleton className="h-[36px] w-[83px] bg-zinc-700" />
       </div>
       <h4 className="my-4 ml-[20px] text-lg">Following games:</h4>
       <Skeleton className="mt-1 h-10 w-[250px] bg-zinc-700" />
-      {/* <Skeleton className="mt-1 h-10 w-[250px] bg-zinc-700" />
-      <Skeleton className="mt-1 h-10 w-[250px] bg-zinc-700" /> */}
-      {/* <Skeleton className="mt-6 h-40 w-[350px] bg-zinc-700" /> */}
     </div>
   );
 }
